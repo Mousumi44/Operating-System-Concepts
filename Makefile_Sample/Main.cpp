6b06b5f81370e08f64bc83e85091cdf520b09ed0
Mousumi44 <54248800+Mousumi44@users.noreply.github.com>
2020-03-08 17:38:50 -0500

    Add files via upload

---
+++ Makefile_Sample/Main.cpp
@@ -0,0 +1,11 @@
+#include<cstdlib>
+#include "message.h"
+
+using namespace std;
+
+int main()
+{
+    message m;
+    m.printMessage();
+    return 0;
+}
---
+++ Makefile_Sample/message.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "message.h"
+
+using namespace std;
+
+void message:: printMessage()
+{
+    cout<<"Makefile Example\n";
+}
