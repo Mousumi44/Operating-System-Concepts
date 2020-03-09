32eb4a888c6c7b43ee1f7f6594d10948d16f79f2
Mousumi44 <54248800+Mousumi44@users.noreply.github.com>
2020-03-09 10:36:47 -0500

    Add files via upload

---
+++ AUBatch-v1/aubatch.h
@@ -0,0 +1,19 @@
+#ifndef AUBATCH_H
+#define AUBATCH_H
+
+/*Lock and condition variables*/
+pthread_mutex_t cmd_queue_lock;  
+pthread_cond_t cmd_buf_not_full; 
+pthread_cond_t cmd_buf_not_empty;
+
+void *scheduling();
+void *dispatching();
+void input_help();
+void input_run(char input_cmd[]);
+void fcfs();
+void sjf();
+void priority();
+void perfEvaluate();
+int splitColon(char time[]);
+
+#endif //AUBATCH_H
